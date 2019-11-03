368ed9dd98f3b8eb5255963b5be1587c31e62abb
Phat Duong <duongp@wit.edu>
2019-11-03 15:24:36 -0500

    multiple plot on a single figure
    multiple plot on a single figure
    need to learn how to do subplot in the future
    change webserver to webserver.py

--- graph.py
+++ graph.py
@@ -4,18 +4,23 @@ import random
 from matplotlib.animation import FuncAnimation
 
 x = []
-y = []
+y1 = []
+y2 = []
 
 index = count()
 
 def _plot(i):
 	x.append(next(index))
-	y.append(random.randint(1,5))
+	y1.append(random.randrange(1,10,1))
+	y2.append(random.randrange(1,10,1))
 
 	plt.cla()
-	plt.plot(x,y)
+	plt.plot(x,y1,label = 'Temperature')
+	plt.plot(x,y2,label = 'Moisture')
+	plt.legend(loc = 'upper right') 
 
-livePlot = FuncAnimation(plt.gcf(),_plot,interval = 1000)
+livePlot = FuncAnimation(plt.gcf(),_plot,interval = 1000*2)
 
 plt.tight_layout()
+
 plt.show()
