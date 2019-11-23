0cb32df06b0ea4dd4dd7e14cbe8bad7afaf90d98
Phat Duong <duongp@wit.edu>
2019-11-22 21:59:41 -0500

    make code looks a bit oker

--- graph.py
+++ graph.py
@@ -3,20 +3,6 @@ import matplotlib.pyplot as plt
 import random
 from matplotlib.animation import FuncAnimation
 
-x = []
-b = [] #brightness
-t = [] #temperature
-m = [] #moisture
-
-index = count()
-fig = plt.figure()
-fig.set_size_inches(11, 6)
-temp = fig.add_subplot(2,2,1)
-moist = fig.add_subplot(2,2,2)
-bright = fig.add_subplot(2,2,3)
-_all = fig.add_subplot(2,2,4)
-plt.subplots_adjust(top=0.9, hspace = 0.4)
-
 def makePlot(i):
 	x.append(next(index))
 	
@@ -41,13 +27,12 @@ def makePlot(i):
 	_all.plot(x,b,label = 'Brightness', color = 'red')
 	_all.plot(x,m,label = 'Moisture', color = 'green')
 	_all.set_title('Everything')
-	_all.legend(loc = 'upper right', prop = {'size': 8})
+	_all.legend(framealpha = 0.2, loc = 'upper right', prop = {'size': 6})
 
 	# _plot(temp,x,t,"Temperature")
 	# _plot(bright,x,b,"Brightness")
 	# _plot(moist,x,m,"Moisture")
 	# plotAll(_all,t,b,m)
-
 
 def _plot(axes,x,y,name):
 	axes.cla()
@@ -61,6 +46,22 @@ def plotAll(axes,x,y1,y2,y3):
 	axes.plot(x,y3,label = "Moisture")
 	axes.legend(loc = "upper right")
 
-livePlot = FuncAnimation(plt.gcf(),makePlot,interval = 1000*1)
+fig = plt.figure()
+fig.set_size_inches(11, 6)
 
+temp = fig.add_subplot(2,2,1)
+moist = fig.add_subplot(2,2,2)
+bright = fig.add_subplot(2,2,3)
+_all = fig.add_subplot(2,2,4)
+
+plt.tight_layout()
+plt.subplots_adjust(top=0.9, hspace = 0.4)
+
+x = [] #time
+b = [] #brightness
+t = [] #temperature
+m = [] #moisture
+index = count()
+
+livePlot = FuncAnimation(plt.gcf(),makePlot,interval = 1000*1)
 plt.show()
