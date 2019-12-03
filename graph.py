1d2d54e1527e544d67f71ac161d37764b35a2b68
Phat Duong <duongp@wit.edu>
2019-12-03 15:18:15 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 from itertools import count
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 import json
 from matplotlib.animation import FuncAnimation
 
@@ -17,29 +18,40 @@ def makePlot(i):
 
 
 	_all.cla()
-	_all.plot(time,t,label = 'Temperature', color = 'blue')
-	_all.plot(time,b,label = 'Brightness', color = 'red')
-	_all.plot(time,m,label = 'Moisture', color = 'green')
+	_all.plot(time,t,label = 'Temperature', color = 'red')
+	_all.plot(time,b,label = 'Brightness', color = 'orange')
+	_all.plot(time,m,label = 'Moisture', color = 'blue')
 	_all.set_title('Everything')
 	_all.legend(framealpha = 0.2, loc = 'upper right', prop = {'size': 6})
-
+	if (len(time)>=16):
+		_all.set_xlim(len(time)-17,len(time)-1)
+		_all.set_xticks(range(len(time)-17,len(time)))
+	
+	_all.grid()
 
 def _plot(axes,x,y,name,_color):
 	axes.cla()
 	axes.plot(x,y,color = _color)
-	axes.set_title(name)
-
+	axes.set_title(name+': '+str(y[len(y)-1]))
+	axes.set_xlabel('Time')
+	axes.set_ylabel(name)
+	if (len(x)>=10):
+		axes.set_xlim(len(x)-11,len(x)-1)
+		axes.set_xticks(range(len(x)-11,len(x)))
+	
 
 fig = plt.figure()
 fig.set_size_inches(11, 6)
 
-temp = fig.add_subplot(2,2,1)
-moist = fig.add_subplot(2,2,2)
-bright = fig.add_subplot(2,2,3)
-_all = fig.add_subplot(2,2,4)
+temp = fig.add_subplot(2,3,4)
+moist = fig.add_subplot(2,3,5)
+bright = fig.add_subplot(2,3,6)
+_all = fig.add_subplot(2,2,1)
+fig.text(10,10,s = 'testing text', fontsize = 13) 
+
 
 plt.tight_layout()
-plt.subplots_adjust(top=0.9, hspace = 0.4)
+plt.subplots_adjust(top=0.9, hspace = 0.4,left=0.05,right=0.95)
 
 time = [] #time
 b = [] #brightness
@@ -47,5 +59,5 @@ t = [] #temperature
 m = [] #moisture
 index = count()
 
-livePlot = FuncAnimation(plt.gcf(),makePlot,interval = 1000*1)
+livePlot = FuncAnimation(fig,makePlot,interval = 1000*1)
 plt.show()
