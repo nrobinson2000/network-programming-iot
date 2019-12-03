8156f411f692788a72c4b72931dea91fe82aea3e
Phat Duong <duongp@wit.edu>
2019-12-03 14:21:18 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,50 +1,34 @@
 from itertools import count
 import matplotlib.pyplot as plt
 import random
+import json
 from matplotlib.animation import FuncAnimation
 
 def makePlot(i):
-	x.append(next(index))
+	time.append(next(index))
 	
 	b.append(random.randrange(1,10,1))
 	t.append(random.randrange(1,10,1))
 	m.append(random.randrange(1,10,1))
+	
+	_plot(axes=temp,x=time,y=t,name = 'Temperature',_color='red')
+	_plot(axes=bright,x=time,y=b,name = 'Brightness',_color= 'orange')
+	_plot(axes=moist,x=time,y=m,name = 'Moisture',_color= 'blue')
 
-	temp.cla()
-	temp.plot(x,t, color = 'blue')
-	temp.set_title('Temperature')
-
-	bright.cla()
-	bright.plot(x,b, color = 'red')
-	bright.set_title('Brightness')
-
-	moist.cla()
-	moist.plot(x,m, color = 'green')
-	moist.set_title('Moisture')
 
 	_all.cla()
-	_all.plot(x,t,label = 'Temperature', color = 'blue')
-	_all.plot(x,b,label = 'Brightness', color = 'red')
-	_all.plot(x,m,label = 'Moisture', color = 'green')
+	_all.plot(time,t,label = 'Temperature', color = 'blue')
+	_all.plot(time,b,label = 'Brightness', color = 'red')
+	_all.plot(time,m,label = 'Moisture', color = 'green')
 	_all.set_title('Everything')
 	_all.legend(framealpha = 0.2, loc = 'upper right', prop = {'size': 6})
 
-	# _plot(temp,x,t,"Temperature")
-	# _plot(bright,x,b,"Brightness")
-	# _plot(moist,x,m,"Moisture")
-	# plotAll(_all,t,b,m)
 
-def _plot(axes,x,y,name):
+def _plot(axes,x,y,name,_color):
 	axes.cla()
-	axes.plot(x,y,label = name)
-	axes.legend(loc = "upper right")
+	axes.plot(x,y,color = _color)
+	axes.set_title(name)
 
-def plotAll(axes,x,y1,y2,y3):
-	axes.cla()
-	axes.plot(x,y1,label = "Temperature")
-	axes.plot(x,y2,label = "Brightness")
-	axes.plot(x,y3,label = "Moisture")
-	axes.legend(loc = "upper right")
 
 fig = plt.figure()
 fig.set_size_inches(11, 6)
@@ -57,7 +41,7 @@ _all = fig.add_subplot(2,2,4)
 plt.tight_layout()
 plt.subplots_adjust(top=0.9, hspace = 0.4)
 
-x = [] #time
+time = [] #time
 b = [] #brightness
 t = [] #temperature
 m = [] #moisture
