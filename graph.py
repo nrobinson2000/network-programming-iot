515863992ec4ecd919268168149e642390734642
Phat Duong <duongp@wit.edu>
2019-12-03 19:55:54 -0500

    graph can now communicate with client

--- graph.py
+++ graph.py
@@ -5,13 +5,20 @@ import numpy as np
 import json
 from matplotlib.animation import FuncAnimation
 
-def makePlot(i):
-	time.append(next(index))
+def makePlot(x,y1,y2,y3):
+	# time.append(next(index))
 	
-	b.append(random.randrange(1,10,1))
-	t.append(random.randrange(1,10,1))
-	m.append(random.randrange(1,10,1))
+	# b.append(random.randrange(1,10,1))
+	# t.append(random.randrange(1,10,1))
+	# m.append(random.randrange(1,10,1))
 
+	global time,t,b,m
+	time = x
+	t = y1
+	b = y2
+	m = y3
+
+	print(time)
 	plotIndividual()
 	plotAll()
 	plotText()
@@ -42,31 +49,32 @@ def plotAll():
 	_all.plot(time,m,label = 'Moisture', color = 'blue')
 	_all.set_title('Everything')
 	_all.legend(loc = (1.01,0.78), prop = {'size': 6})
+	_all.set_xticks(time)
 	if (len(time)>15):
 		_all.set_xlim(len(time)-16,len(time)-1)
-		_all.set_xticks(range(len(time)-16,len(time)))
 	else:
-		_all.set_xlim(0,15)
-		_all.set_xticks(range(0,16))
+		_all.set_xlim(0,time[len(time)-1])
 	_all.grid()
 
 def _plot(axes,x,y,name,_color):
 	axes.cla()
 	axes.plot(x,y,color = _color)
-	axes.set_title(name+': '+str(y[len(y)-1]))
+	axes.set_title(name) #+': '+str(y[len(y)-1]))
 	axes.set_xlabel('Time')
 	axes.set_ylabel(name)
+	axes.set_xticks(x)
 	if (len(x)>10):
 		axes.set_xlim(len(x)-11,len(x)-1)
-		axes.set_xticks(range(len(x)-11,len(x)))
 	else:
-		axes.set_xlim(0,10)
-		axes.set_xticks(range(0,11))
+		axes.set_xlim(0,x[len(x)-1])
 	axes.grid()
 	
-def graph():
-	livePlot = FuncAnimation(fig,makePlot,interval = 1000*1)
+def graph(x,y1,y2,y3):
+	
+	# livePlot = FuncAnimation(fig,makePlot,interval = 1000*1)
+	makePlot(x=x,y1=y1,y2=y2,y3=y3)
 	plt.show()
+		
 
 time = [] #time
 b = [] #brightness
@@ -87,4 +95,6 @@ txt.get_yaxis().set_visible(False)
 for i in txt.spines:
 	txt.spines[i].set_visible(False)
 plt.subplots_adjust(top=0.9, hspace = 0.4,left=0.05,right=0.95)
-graph()
+
+graph(x=range(20),y1=range(1,21),y2=range(21,41),y3=range(41,61))
+print(time)
